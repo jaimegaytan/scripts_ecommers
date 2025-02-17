0ba477e95b6096a2fd1ccd60f321e445625a4536
jaimegaytan <167459574+jaimegaytan@users.noreply.github.com>
2025-02-17 12:56:11 -0600

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+
+module.exports = defineConfig({
+  projectId: 'vyb2np',  
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    
+    pageLoadTimeout: 120000
+
+  },
+});
